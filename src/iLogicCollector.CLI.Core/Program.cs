f43a2a2f05e49c55ce39a73c770486bb9f0be402
mjordan <jordan@scenic-shop.com>
2020-01-17 01:48:42 -0500

    Started roughing in CLI version.

--- src/iLogicCollector.CLI.Core/Program.cs
+++ src/iLogicCollector.CLI.Core/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace iLogicCollector.CLI.Core
 {
@@ -7,6 +9,29 @@ namespace iLogicCollector.CLI.Core
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            //get parameters
+            //-path (optional, or use cwd), -file (optional, file name to use)
+
+            String directory = "stuff"; //set directory based on parameter match
+            String filename = "stuff"; //set filename
+            String filePath = directory + "/" + filename + ".iLogicVB";
+            var buffer = new List<String>();
+
+            //Get list of files in directory
+            string[] fileList = System.IO.Directory.GetFiles(directory);
+
+            // ignore binary files (only look into text files?)
+            foreach (var i in fileList) 
+            {
+                //get the file lines and read the first few to test.
+                //search through files, look for matches to the tags.
+                //add to the buff if found.
+                if (Regex.IsMatch(i, "(?i)</iLogicCollectorHeader>"))
+                {
+                    ''
+                }
+            }
         }
     }
 }
