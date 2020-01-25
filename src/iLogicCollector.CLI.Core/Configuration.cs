61d7025478b7103cfe407b0ea8dbe478280e1595
mjordan <meloncholia@gmail.com>
2020-01-25 18:02:11 -0500

    Moved configuration assignment ot Configuration class construcor.

--- src/iLogicCollector.CLI.Core/Configuration.cs
+++ src/iLogicCollector.CLI.Core/Configuration.cs
@@ -1,4 +1,8 @@
-﻿namespace iLogicCollector
+﻿using System;
+using System.IO;
+using iLogicCollector.Utility;
+
+namespace iLogicCollector
 {
     public class Configuration
     {
@@ -8,6 +12,38 @@
         public string Target;
         public bool Force = false;
 
-        public void New() { }
+        public Configuration(string[] args)
+        {
+            // Command line parsing
+            var parameters = new Arguments(args);
+
+            //get parameters
+            //-path (optional, or use cwd), -output (optional, file name to use), -config (optional, configuration tag to recognize), -force/-f (optional, forces the overwrite of the target file.)
+
+            // Look for specific arguments values and display them if they exist (return null if they don't)
+            if ((parameters["path"] != null) || (parameters["p"] != null))
+            { CollectPath = parameters["path"]; }
+            else
+            { CollectPath = Environment.CurrentDirectory; }
+
+            if ((parameters["output"] != null) || (parameters["o"] != null))
+            { File = parameters["File"]; }
+            else
+            {
+                var temp = Path.GetDirectoryName(Environment.CurrentDirectory);
+                File = temp + ".iLogicVb";
+            }
+
+            if ((parameters["help"] != null) || (parameters["h"] != null))
+            {
+                //show the help menu;
+            }
+
+            if ((parameters["f"] != null) || (parameters["force"] != null))
+            {
+                Force = true;
+            }
+
+        }
     }
 }
--- src/iLogicCollector.CLI.Core/Program.cs
+++ src/iLogicCollector.CLI.Core/Program.cs
@@ -13,35 +13,8 @@ namespace iLogicCollector
         {
             Console.WriteLine("Hello World!");
 
-            // Command line parsing
-             var parameters = new Arguments(args);
-             var config = new Configuration();
-            //get parameters
-            //-path (optional, or use cwd), -output (optional, file name to use), -config (optional, configuration tag to recognize), -force/-f (optional, forces the overwrite of the target file.)
-
-            // Look for specific arguments values and display them if they exist (return null if they don't)
-            if ((parameters["path"] != null) || (parameters["p"] != null))
-            { config.CollectPath = parameters["path"];}
-            else
-            {config.CollectPath = Environment.CurrentDirectory;}
-
-            if ((parameters["output"] != null) || (parameters["o"] != null))
-            { config.File = parameters["File"]; }
-            else
-            {
-                var temp = Path.GetDirectoryName(Environment.CurrentDirectory);
-                config.File = temp + ".iLogicVb";
-            }
-
-            if ((parameters["help"] != null) || (parameters["h"] != null))
-            {
-                //show the help menu;
-            }
-
-            if ((parameters["f"] != null) || (parameters["force"] != null))
-            {
-                config.Force = true;
-            }
+ 
+             var config = new Configuration(args);
 
         }
 
