9ff735db016f001f76c5d2bbc311a8e9e6ec254c
mjordan <meloncholia@gmail.com>
2020-01-25 17:52:28 -0500

    Added parameter parser.

--- src/iLogicCollector.CLI.Core/Program.cs
+++ src/iLogicCollector.CLI.Core/Program.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
+using iLogicCollector.Utility;
 
-namespace iLogicCollector.CLI.Core
+namespace iLogicCollector
 {
     class Program
     {
@@ -12,76 +13,49 @@ namespace iLogicCollector.CLI.Core
         {
             Console.WriteLine("Hello World!");
 
+            // Command line parsing
+             var parameters = new Arguments(args);
+             var config = new Configuration();
             //get parameters
-            //-path (optional, or use cwd), -file (optional, file name to use), -config (optional, configuration tag to recognize)
+            //-path (optional, or use cwd), -output (optional, file name to use), -config (optional, configuration tag to recognize), -force/-f (optional, forces the overwrite of the target file.)
 
+            // Look for specific arguments values and display them if they exist (return null if they don't)
+            if ((parameters["path"] != null) || (parameters["p"] != null))
+            { config.CollectPath = parameters["path"];}
+            else
+            {config.CollectPath = Environment.CurrentDirectory;}
 
-            if (args == null)
+            if ((parameters["output"] != null) || (parameters["o"] != null))
+            { config.File = parameters["File"]; }
+            else
             {
-                Console.WriteLine("args is null");
+                var temp = Path.GetDirectoryName(Environment.CurrentDirectory);
+                config.File = temp + ".iLogicVb";
             }
 
-
-            ArgumentParser parser = new ArgumentParser(args);
-
-            String directory = "stuff"; //set directory based on parameter match
-            String filename = "stuff"; //set filename
-            String filePath = directory + "/" + filename + ".iLogicVB";
-            var buffer = new List<String>();
-
-            //Get list of files in directory
-            string[] fileList = System.IO.Directory.GetFiles(directory);
-
-            // ignore binary files (only look into text files?)
-            foreach (var i in fileList) 
+            if ((parameters["help"] != null) || (parameters["h"] != null))
             {
-                //get the file lines and read the first few to test.
-                //search through files, look for matches to the tags.
-                //add to the buff if found.
-                if (Regex.IsMatch(i, "(?i)</iLogicCollectorHeader>"))
-                {
-                    //do something
-                }
+                //show the help menu;
             }
+
+            if ((parameters["f"] != null) || (parameters["force"] != null))
+            {
+                config.Force = true;
+            }
+
         }
 
-    }
-
-    public class ArgumentParser
-    {
-        public Dictionary<string, string> ParameterList { get; set; } = new Dictionary<string, string>(;
-
-        public void ArgumentParser(string[] keys]) 
-        {
-            foreach (string i in keys)
-            {
-                ParameterList.add(i, "");
-            }
         }
-        public void Parse(string[] args)
-        {
-            while (args.Any())
-            {
-                if (ParameterList.ContainsKey(args[0]) && !ParameterList.ContainsKey(args[1]))
-                {
-                    ParameterList[args[0]] = args[1];
-                    args = args.Skip(2).ToArray();
-                }
-                else
-                {
-                    InvalidDataException err;
-                    throw new System.ArgumentException("Input arguments were not recognized.");
-                }
-            }
-        }
-}
 
-    public class ConfigurationPojo
+
+    public class Configuration
     {
         public string CollectPath;
         public string File;
         public string FilePath;
         public string Target;
+        public bool Force = false;
 
+        public void New() { }
     }
 }
