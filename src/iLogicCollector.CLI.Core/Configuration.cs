d12f13f32d94f453a9ef93f08e507fbbf8b57afa
mjordan <meloncholia@gmail.com>
2020-01-25 17:55:18 -0500

    Extracted Configuration.cs

---
+++ src/iLogicCollector.CLI.Core/Configuration.cs
@@ -0,0 +1,13 @@
+﻿namespace iLogicCollector
+{
+    public class Configuration
+    {
+        public string CollectPath;
+        public string File;
+        public string FilePath;
+        public string Target;
+        public bool Force = false;
+
+        public void New() { }
+    }
+}
--- src/iLogicCollector.CLI.Core/Program.cs
+++ src/iLogicCollector.CLI.Core/Program.cs
@@ -46,16 +46,4 @@ namespace iLogicCollector
         }
 
         }
-
-
-    public class Configuration
-    {
-        public string CollectPath;
-        public string File;
-        public string FilePath;
-        public string Target;
-        public bool Force = false;
-
-        public void New() { }
-    }
 }
