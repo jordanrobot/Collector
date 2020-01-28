5072cfbb36fefb26f4bff5538c8b454932b4a36e
mjordan <meloncholia@gmail.com>
2020-01-27 21:09:28 -0500

    - Added Input propoerty - Added CalculateInputPath() - fixed bug in input and output code blocks.

--- src/iLogicCollector.CLI.Core/Configuration.cs
+++ src/iLogicCollector.CLI.Core/Configuration.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 using System.IO;
 using System.Reflection;
 
@@ -6,13 +7,13 @@ namespace iLogicCollector
 {
     public class Config
     {
-        public string CollectPath { get; set; }
+        public string Input { get; set; }
         public string Output { get; set; } //user entered output (file/path) name to save the collected file to
         public string FilePath { get; set; } //formatted and corrected output filename
         public bool Force { get; set; } = false;
         public bool Debug { get; set; } = false;
         public static string Version { get; }= Assembly.GetEntryAssembly().GetName().Version.ToString();
-
+        public string CurrentWorkingPath { get; } = Path.GetDirectoryName(Environment.CurrentDirectory);
 
         public Config(string[] args)
         {
@@ -23,20 +24,28 @@ namespace iLogicCollector
             //-path (optional, or use cwd), -output (optional, file name to use), -config (optional, configuration tag to recognize), -force/-f (optional, forces the overwrite of the target file.)
 
             // Look for specific arguments values and display them if they exist (return null if they don't)
-            //Path
-            if ((parameters["path"] != null) || (parameters["p"] != null))
-            { CollectPath = parameters["path"]; }
+            //Input
+            if ((parameters["input"] != null) || (parameters["i"] != null))
+            {
+                var tempInput = parameters["input"] ?? parameters["i"];
+                this.Input = CalculateInputPath(tempInput);
+            }
             else
-            { CollectPath = Environment.CurrentDirectory; }
+            {
+                this.Input = Environment.CurrentDirectory;
+            }
+
 
             //Output
-            if ((parameters["output"] != null) || (parameters["o"] != null))
-            { Output = parameters["output"]; }
-            else
+            if ((parameters["output"] == null) && (parameters["o"] == null))
             {
                 //Todo: make the path/output recognize if the entered parameter is a whole path, or just a file name.
                 var temp = Path.GetDirectoryName(Environment.CurrentDirectory);
                 Output = temp + ".iLogicVb";
+            }
+            else
+            {
+                Output = parameters["output"] ?? parameters["o"];
             }
 
             //Help;
@@ -68,6 +77,54 @@ namespace iLogicCollector
             //Todo: set the rest of these config options
 
 
+        }
+
+        public string CalculateInputPath(string input)
+        {
+ 
+            // Is directory null?
+            if (string.IsNullOrEmpty(input))
+            {
+                if (this.Debug == true) { Console.WriteLine("Input parameter directory is null or empty."); }
+                return CurrentWorkingPath + Path.DirectorySeparatorChar;
+            }
+
+            // Does directory exist?
+            if (Directory.Exists(input))
+            {
+                //Console.WriteLine("Input parameter directory exists...");
+                return input;
+            }
+
+            // Check if directory is complete, but does not exists.  This should return an error.
+            if (Path.IsPathRooted(input))
+            {
+                //Console.WriteLine("Input parameter directory does not exist, we're going to exit roughly.");
+                throw new SystemException("The input path does not exist.  Exiting.");
+            }
+
+            //Does the input end in a '\' character?  If not, add one...
+            var tempPath = CurrentWorkingPath + Path.DirectorySeparatorChar + input;
+            if (input.EndsWith(@"\", StringComparison.CurrentCulture))
+            {
+                //Console.WriteLine(@"Input parameter - assume it's relative to cwd.  Need to add a \ character");
+                input = input + @"\";
+            }
+
+            // Check if C:/Current/working/directory/inputpath/ exists...
+            if (Directory.Exists(tempPath))
+            {
+                if (this.Debug == true) { Console.WriteLine("Input parameter directory, assume relative... it's found!"); }
+                return tempPath;
+            }
+
+            if (this.Debug == true) { Console.WriteLine("Input parameter directory could not be determined.  Exit."); }
+            return null;
+        }
+
+        public string CalculateOutputPath(string output)
+        {
+            return null;
         }
     }
 }
