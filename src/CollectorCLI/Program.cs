67154b52d6dfe2087774bce68ce1752a24c325c5
mjordan <meloncholia@gmail.com>
2020-04-06 21:13:32 -0400

    Added ShowDebugOutput()

--- src/CollectorCLI/Program.cs
+++ src/CollectorCLI/Program.cs
@@ -14,18 +14,21 @@ namespace Collector
 
             if (config.Debug == true)
             {
-
-                Console.Write("\n-------------\n Debugging Dump:\n");
-                Console.WriteLine("Input =      " + config.Input);
-                Console.WriteLine("Output =     " + config.Output);
-                Console.WriteLine("Force =      " + config.Force);
-                Console.WriteLine("Debug =      "+ config.Debug);
-                Console.WriteLine("Recursion =  "+ config.Recursion);
-                Console.WriteLine("CWD =        "+ config.CurrentWorkingPath);
+                ShowDebugOutput(config);
             }
 
             return;
         }
 
+        private static void ShowDebugOutput(Configuration config)
+        {
+            Console.Write("\n-------------\n Debugging Dump:\n");
+            Console.WriteLine("Input    = " + config.Input);
+            Console.WriteLine("Output   = " + config.Output);
+            Console.WriteLine("Force    = " + config.Force);
+            Console.WriteLine("Debug    = " + config.Debug);
+            Console.WriteLine("Recursion= " + config.Recursion);
+            Console.WriteLine("CWD      = " + config.CurrentWorkingPath);
         }
+    }
 }
